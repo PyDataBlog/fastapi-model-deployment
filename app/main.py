c879a08c2d41327088257f0b8edaf5f9d1ca7da5
Bernard Brenyah <pimpfada@gmail.com>
2019-10-28 16:45:58 +0100

    initiated logging functionality

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 from data.predictions_handler import get_predictions
-
+from loguru import logger
 
 # Initiate app instance
 app = FastAPI(title='Forged Or Not Forged', version='1.0',
@@ -19,6 +19,11 @@ class Features(BaseModel):
     curtosis_of_wavelet: float
     entropy_of_wavelet: float
 
+# Initiate logging
+log_format = ""
+logger.add(sink='/data/log_files/log.log', format=log_format, level='DEBUG')
+
+
 # Api root/home
 @app.get('/')
 @app.get('/home')
@@ -29,10 +34,13 @@ def read_home():
 # Prediction endpoint
 @app.post('/predict')
 def get_prediction(incoming_data: Features):
+    # retrieve incoming json data as a dictionary
     new_data = incoming_data.dict()
 
+    # Make predictions based on the incoming data and saved neural net
     preds = get_predictions(new_data)
 
+    # Return the predicted class and the predicted probability
     return {'predicted_class': round(float(preds.flatten())), 'predicted_prob': float(preds.flatten())}
 
 
