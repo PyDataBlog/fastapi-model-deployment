e45db8407da1c29f2f34ba3becf9c1a9667a5e54
Bernard Brenyah <pimpfada@gmail.com>
2019-10-29 11:49:50 +0100

    cleaned up and added docs

--- app/data/predictions_handler.py
+++ app/data/predictions_handler.py
@@ -3,20 +3,33 @@ import pandas as pd
 import numpy as np
 import joblib
 import os
+from loguru import logger
 
 
+@logger.catch
 def get_predictions(data):
-    print(os.getcwd())
-    new_data = {k:[v] for k, v in data.items()}
+    """
+    A function that reshapes the incoming JSON data, loads the saved model objects
+    and returns predicted class and probability.
+
+    :param data: Dict with keys representing features and values representing the associated value
+    :return: Dict with keys 'predicted_class' (class predicted) and 'predicted_prob' (probability of prediction)
+    """
+    new_data = {k: [v] for k, v in data.items()}
 
     new_data_df = pd.DataFrame.from_dict(new_data)
     new_data_df = new_data_df[['variance_of_wavelet', 'skewness_of_wavelet',
                                'curtosis_of_wavelet', 'entropy_of_wavelet']]
 
     scaler = joblib.load('app/data/scaler.joblib')
+    logger.debug('Saved standardising object successfully loaded')
+
     model = tf.keras.models.load_model('app/data/banknote_authentication_model.h5')
+    logger.debug('Saved ANN model loaded successfully')
 
     X = scaler.transform(new_data_df.values)
+    logger.debug('Incoming data successfully standardised with saved object')
+
     preds = model.predict(X)
 
     return preds
--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 from data.predictions_handler import get_predictions
 from loguru import logger
 
+
 # Initiate app instance
 app = FastAPI(title='Forged Or Not Forged', version='1.0',
               description='A simple Neural Network that classifies a banknote as either forged or not')
@@ -19,26 +20,42 @@ class Features(BaseModel):
     curtosis_of_wavelet: float
     entropy_of_wavelet: float
 
+
 # Initiate logging
-log_format = ""
-logger.add(sink='/data/log_files/log.log', format=log_format, level='DEBUG')
+log_format = "{time} | {level} | {message} | {file} | {line} | {function} | {exception}"
+logger.add(sink='app/data/log_files/logs.log', format=log_format, level='DEBUG', compression='zip')
 
 
-# Api root/home
+# Api root or home endpoint
 @app.get('/')
 @app.get('/home')
 def read_home():
+    """
+    Home endpoint which can be used to test the availability of the application.
+
+    :return: Dict with key 'message' and value 'Fake or Not API live!'
+    """
+    logger.debug('User checked the root page')
     return {'message': 'Fake or Not API live!'}
 
 
 # Prediction endpoint
 @app.post('/predict')
+@logger.catch()  # catch any unexpected breaks
 def get_prediction(incoming_data: Features):
+    """
+    This endpoint serves the predictions based on the values received from a user and the saved model.
+
+    :param incoming_data: JSON with keys representing features and values representing the associated values.
+    :return: Dict with keys 'predicted_class' (class predicted) and 'predicted_prob' (probability of prediction)
+    """
     # retrieve incoming json data as a dictionary
     new_data = incoming_data.dict()
+    logger.info('User sent some data for predictions')
 
     # Make predictions based on the incoming data and saved neural net
     preds = get_predictions(new_data)
+    logger.debug('Predictions successfully generated for the user')
 
     # Return the predicted class and the predicted probability
     return {'predicted_class': round(float(preds.flatten())), 'predicted_prob': float(preds.flatten())}
